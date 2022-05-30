f518f9cab3a8e13be1e5e8485697f7bac4d2db56
Daniil Simon <simonda6@fel.cvut.cz>
2022-05-30 11:00:31 +0200

    edit routes

--- webpack-base.config.js
+++ webpack-base.config.js
@@ -26,7 +26,7 @@ const getWebpackConfig = (extraConfig = {}) => {
             entry: resolve(__dirname, "src", "index.js"),
             output: {
                 path: resolve(__dirname, "dist"),
-                publicPath: isLocal ? "/" : "/",
+                publicPath: isLocal ? "/" : "/Dansii",
                 filename: isLocal
                     ? "[name].[hash].js"
                     : "[name].[contenthash].js",
