2fd7a85033dedc7062567b6482991d3be6df900f
Daniil Simon <simonda6@fel.cvut.cz>
2022-05-30 04:00:46 +0200

    edit routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {SignInForm} from "./domains/sing-in-form/sign-in-form";
 import {SignUpForm} from "./domains/sign-up-form/sign-up-form";
-import {Switch, Route , useHistory} from "react-router-dom";
+import {Switch, Route, useHistory} from "react-router-dom";
 import {Dashboard} from "./domains/dashboard/dashboard";
 import {HistoryGate} from "./domains/history/store";
 
@@ -11,14 +11,11 @@ export const App = () => {
     return (
         <>
             <Switch>
-                <Route path={'/reservation-system-kaj'} exact={true}>
-                    <SignInForm/>
+                <Route path={'/reservation-system-kaj/sign-in'} exact={true} component={SignInForm}>
                 </Route>
-                <Route path={'/reservation-system-kaj/sign-up'}>
-                    <SignUpForm/>
+                <Route path={'/reservation-system-kaj/sign-up'} component={SignUpForm}>
                 </Route>
-                <Route path={'/reservation-system-kaj/dashboard'} exact={true}>
-                    <Dashboard/>
+                <Route path={'/reservation-system-kaj/dashboard'} exact={true} component={Dashboard}>
                 </Route>
             </Switch>
             <HistoryGate history={history} />
